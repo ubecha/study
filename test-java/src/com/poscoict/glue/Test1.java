1003f1f667da8a8c3ca099d1f853528a4587dd1a
장상훈(협력사) <ubecha@poscoict.com>
2019-01-16 16:55:01 +0900

    create new branch test

--- test-java/src/com/poscoict/glue/Test1.java
+++ test-java/src/com/poscoict/glue/Test1.java
@@ -4,7 +4,8 @@ public class Test1 {
 
     public static void main( String[] args ) {
         // TODO Auto-generated method stub
-        System.out.println( "Hello World" );
+        System.out.println( "Hello World 1" );
+        System.out.println( "Hello World 2" );
     }
 
 }
